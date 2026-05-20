d07e14518898e0139e43f3bb7edf9b95b593d622
ianavila-1 <ia.avilam@duocuc.cl>
2026-05-20 03:28:01 -0400

    Update WebClientConfig.java

--- ms.compras/src/main/java/cl/duoc/tienda/ms/compras/config/WebClientConfig.java
+++ ms.compras/src/main/java/cl/duoc/tienda/ms/compras/config/WebClientConfig.java
@@ -14,4 +14,18 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
+  @Value("${servicios.timeout.segundos:5}")
+    private int timeoutSegundos;
+
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeoutSegundos * 1000)
+                .doOnConnected(conn -> conn
+                        .addHandlerLast(new ReadTimeoutHandler(timeoutSegundos, TimeUnit.SECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(timeoutSegundos, TimeUnit.SECONDS)));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
 }
