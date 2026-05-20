757392b34f5f85cc4a9e635375d19d1897767cf1
ianavila-1 <ia.avilam@duocuc.cl>
2026-05-20 02:53:26 -0400

    Update BibliotecaController.java

--- ms.compras/src/main/java/cl/duoc/tienda/ms/compras/controller/BibliotecaController.java
+++ ms.compras/src/main/java/cl/duoc/tienda/ms/compras/controller/BibliotecaController.java
@@ -12,4 +12,15 @@ import java.util.List;
 @RequestMapping("/api/biblioteca")
 @RequiredArgsConstructor
 public class BibliotecaController {
+  private final BibliotecaService service;
+
+    @GetMapping("/{usuarioId}")
+    public ResponseEntity<List<JuegoEnBibliotecaDTO>> biblioteca(@PathVariable Long usuarioId) {
+        return ResponseEntity.ok(service.bibliotecaDe(usuarioId));
+    }
+
+    @GetMapping("/{usuarioId}/{juegoId}")
+    public ResponseEntity<JuegoEnBibliotecaDTO> verificar(@PathVariable Long usuarioId, @PathVariable Long juegoId) {
+        return ResponseEntity.ok(service.verificarPosesion(usuarioId, juegoId));
+    }
 }
