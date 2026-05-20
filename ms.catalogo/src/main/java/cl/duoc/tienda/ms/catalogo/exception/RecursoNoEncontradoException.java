9aa7315100a228222881c25a18143537ebaa24b7
eln4ch0-0 <ig.veral@duocuc.cl>
2026-05-20 03:01:25 -0400

    Add constructor to RecursoNoEncontradoException

--- ms.catalogo/src/main/java/cl/duoc/tienda/ms/catalogo/exception/RecursoNoEncontradoException.java
+++ ms.catalogo/src/main/java/cl/duoc/tienda/ms/catalogo/exception/RecursoNoEncontradoException.java
@@ -1,4 +1,5 @@
 package cl.duoc.tienda.ms.catalogo.exception;
 
 public class RecursoNoEncontradoException extends RuntimeException {
+  public RecursoNoEncontradoException(String msg) { super(msg); }
 }
