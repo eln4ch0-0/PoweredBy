0d57a9ace18d9a22f370f6baab05bc41bde68e9b
eln4ch0-0 <ig.veral@duocuc.cl>
2026-05-20 03:02:54 -0400

    Enhance Genero class with id and attributes
    Added fields for id, nombre, and descripcion to the Genero class.

--- ms.catalogo/src/main/java/cl/duoc/tienda/ms/catalogo/model/Genero.java
+++ ms.catalogo/src/main/java/cl/duoc/tienda/ms/catalogo/model/Genero.java
@@ -11,4 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Genero {
+  
+  @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
 }
