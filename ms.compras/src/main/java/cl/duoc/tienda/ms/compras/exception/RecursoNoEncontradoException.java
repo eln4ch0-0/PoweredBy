d9016d922435668f790b5abb42f2ccee47109b22
ianavila-1 <ia.avilam@duocuc.cl>
2026-05-20 03:31:53 -0400

    Update RecursoNoEncontradoException.java

--- ms.compras/src/main/java/cl/duoc/tienda/ms/compras/exception/RecursoNoEncontradoException.java
+++ ms.compras/src/main/java/cl/duoc/tienda/ms/compras/exception/RecursoNoEncontradoException.java
@@ -1,4 +1,5 @@
 package cl.duoc.tienda.ms.compras.exception;
 
 public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String msg) { super(msg); }
 }
