69600064927b30567de70484577d62a0706926ce
ianavila-1 <ia.avilam@duocuc.cl>
2026-05-20 03:28:29 -0400

    Update CompraRequestDTO.java

--- ms.compras/src/main/java/cl/duoc/tienda/ms/compras/dto/CompraRequestDTO.java
+++ ms.compras/src/main/java/cl/duoc/tienda/ms/compras/dto/CompraRequestDTO.java
@@ -7,4 +7,11 @@ import lombok.Data;
 @Data
 public class CompraRequestDTO {
 
+    @NotNull(message = "El usuarioId es obligatorio")
+    @Positive
+    private Long usuarioId;
+
+    @NotNull(message = "El juegoId es obligatorio")
+    @Positive
+    private Long juegoId;
 }
