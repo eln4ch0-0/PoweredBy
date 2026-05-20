686d7d366df1c96160f0f985517963e5d09f919a
ianavila-1 <ia.avilam@duocuc.cl>
2026-05-20 02:54:02 -0400

    Update CompraController.java

--- ms.compras/src/main/java/cl/duoc/tienda/ms/compras/controller/CompraController.java
+++ ms.compras/src/main/java/cl/duoc/tienda/ms/compras/controller/CompraController.java
@@ -15,4 +15,25 @@ import java.util.List;
 @RequestMapping("/api/compras")
 @RequiredArgsConstructor
 public class CompraController {
+   private final CompraService service;
+
+    @PostMapping
+    public ResponseEntity<CompraResponseDTO> realizar(@Valid @RequestBody CompraRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.realizarCompra(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CompraResponseDTO>> listar() {
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompraResponseDTO> obtener(@PathVariable Long id) {
+        return ResponseEntity.ok(service.obtener(id));
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<CompraResponseDTO>> historial(@PathVariable Long usuarioId) {
+        return ResponseEntity.ok(service.historialUsuario(usuarioId));
+    }
 }
